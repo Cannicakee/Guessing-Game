bdc3f73436c72f7b2e56e244bfb1dd6edcc12cca
Cannicakee <stanfill11@gmail.com>
2024-04-17 22:02:44 -0600

    askRange Question

--- aa03-guessing-game/guessing-game-project/guessing-game.js
+++ aa03-guessing-game/guessing-game-project/guessing-game.js
@@ -1,6 +1,9 @@
 
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
+const { rejects } = require('node:assert');
+const { resolve } = require('node:path');
+const { resolveCaa } = require('node:dns');
 
 const rl = readline.createInterface({ input, output });
 
@@ -20,7 +23,30 @@ function askGuess() {
         }
     });
 }
-askGuess()
+// askGuess()
+const minNumber = () => {
+    return new Promise((resolve, reject) => {
+        rl.question("Enter a minimum number: ", (minAnswer) => {
+            console.log(Number(minAnswer))
+            resolve();
+        })
+    })
+}
+const maxNumber = () => {
+    return new Promise((resolve, reject) => {
+        rl.question("Enter a maximum number: ", (maxAnswer) => {
+            console.log(Number(maxAnswer))
+            resolve();
+        })
+    })
+}
+const askRange = async () => {
+    await minNumber()
+    await maxNumber()
+    console.log(`I'm thinking of a number between ${minNumber} and ${maxNumber}`)
+    rl.close()
+}
+askRange();
 
 let secretnumber = randomInRange(0, 100);
 
@@ -38,3 +64,4 @@ const checkGuess = num => {
         return true;
     }
 }
+
