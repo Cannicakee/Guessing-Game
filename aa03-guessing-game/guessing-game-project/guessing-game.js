ccd49f70731bb6a1daba4d25067538a7c7f39edb
Manni <emmanuelmelgoza1@gmail.com>
2024-04-17 21:27:39 -0600

    random module

--- aa03-guessing-game/guessing-game-project/guessing-game.js
+++ aa03-guessing-game/guessing-game-project/guessing-game.js
@@ -4,6 +4,12 @@ const { stdin: input, stdout: output } = require('node:process');
 
 const rl = readline.createInterface({ input, output });
 
+const randomInRange = (minNum, maxNum) => {
+    const minCeiled = Math.ceil(minNum);
+    const maxFloored = Math.floor(maxNum);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+};
+
 function askGuess() {
     rl.question('Enter a guess: ', (answer) => {
         if (checkGuess(Number(answer))) {
@@ -15,7 +21,9 @@ function askGuess() {
     });
 }
 askGuess()
-let secretnumber = 3;
+
+let secretnumber = randomInRange(0, 100);
+
 const checkGuess = num => {
     if (secretnumber < num) {
         console.log("Too high.")
