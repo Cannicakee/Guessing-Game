3bd167bef6c4a7a8301c7fcbe315954ef0ddfbce
Cannicakee <stanfill11@gmail.com>
2024-04-18 15:30:51 -0600

    Completed Guessing Game

--- aa03-guessing-game/guessing-game-project/guessing-game.js
+++ aa03-guessing-game/guessing-game-project/guessing-game.js
@@ -1,57 +1,58 @@
 
 const readline = require('node:readline');
 const { stdin: input, stdout: output } = require('node:process');
-// const { rejects } = require('node:assert');
-// const { resolve } = require('node:path');
-// const { resolveCaa } = require('node:dns');
 
 const rl = readline.createInterface({ input, output });
-//ASKRANGE QUESTIONS START
 
-
-function askRange() {
-    rl.question('Enter Min: ', (minAnswer) => {
-        rl.question('Enter Max: ', (maxAnswer) => {
-            rl.close();
-        });
-    });
-    const randomInRange = (minNum, maxNum) => {
-        const minCeiled = Math.ceil(minNum);
-        const maxFloored = Math.floor(maxNum);
-        return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
-    };
-
-    let secretnumber = randomInRange();
-
-    const checkGuess = num => {
-        if (secretnumber < num) {
-            console.log("Too high.")
-            return false;
+const randomInRange = (minNum, maxNum) => {
+    const minCeiled = Math.ceil(minNum);
+    const maxFloored = Math.floor(maxNum);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled);
+};
+function askLimit() {
+    rl.question('Enter Turn Limit: ', (answer) => {
+        console.log(`*${answer}*`)
+        let numAttempts = answer;
+        function askRange() {
+            rl.question('Enter Min: ', (minAnswer) => {
+                console.log(`*${minAnswer}*`)
+                rl.question('Enter Max: ', (maxAnswer) => {
+                    console.log(`*${maxAnswer}*`)
+                    const secretnumber = randomInRange(Number(minAnswer), Number(maxAnswer))
+                    askGuess(secretnumber)
+                    function checkGuess(num) {
+                        if (secretnumber < num) {
+                            console.log("Too high.");
+                            return false;
+                        }
+                        if (secretnumber > num) {
+                            console.log("Too low.");
+                            return false;
+                        }
+                        if (secretnumber === num) {
+                            console.log("Correct!");
+                            return true;
+                        }
+                    }
+                    function askGuess() {
+                        rl.question('Enter a guess: ', (answer) => {
+                            if (checkGuess(Number(answer))) {
+                                console.log('You Win')
+                                rl.close();
+                            } else {
+                                numAttempts--;
+                                if (numAttempts === 0) {
+                                    console.log("You lose")
+                                    rl.close();
+                                }
+                                askGuess()
+                            }
+                        });
+                    }
+                });
+            });
         }
-        if (secretnumber > num) {
-            console.log("Too low.")
-            return false;
-        }
-        if (secretnumber === num) {
-            console.log("Correct!")
-            return true;
-        }
-    }
-
-    //ASKRANGE QUESTIONS END
-
-
-    //ASKGUESS FUNCTION START
-    function askGuess() {
-        rl.question('Enter a guess: ', (answer) => {
-            if (checkGuess(Number(answer))) {
-                console.log('You Win')
-                rl.close();
-            } else {
-                askGuess()
-            }
-        });
-    }
-    askGuess()
+        askRange()
+    })
 }
-askRange()
+askLimit()
